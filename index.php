deff57ebfc362017a319a5345e57a79236cda948
nobody08 <111040450+nobody08@users.noreply.github.com>
2022-08-28 12:18:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>voting</title>
+    
+    <!-- Bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body class="bg-dark">
+    <h1 class="text-info text-center p-3">Voting</h1>
+    <div class="bg-info py-4">
+        <h2 class="text-center">Login</h2>
+        <div class="container text-center">
+            <from action="../action/register.php" method="POST" 
+            enctype="multipart/form-data">
+                <div class="mb-3">
+                <input type="text" class="form-control w-50 m-auto" 
+                name="username" placeholder="Enter your username" required="required">
+                </div>
+                <div class="mb-3">
+                <input type="password" class="form-control w-50 m-auto" 
+                name="password" placeholder="Enter your password" required="required">
+                </div>
+                <div class="mb-3">
+                    <select name="std" class="form-select w-50 m-auto">
+                        <option value="group">Group</option>
+                        <option value="voter">Voter</option>
+                    </select> 
+                 </div>
+                 <button type="submit" class="btn btn-dark my-4">Login</button>
+                 <p>Don't have an account?<a href="./partials/registration.php" class="text-white">Register here</a></p>
+          </form>
+        </div>   
+    </div>
+
+</body>
+</html>
